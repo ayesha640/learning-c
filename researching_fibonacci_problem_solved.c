82ec47c3eb3550d0a0c2c0b5c1db15baa098cdb5
ayesha640 <159019194+ayesha640@users.noreply.github.com>
2024-04-04 20:37:14 +0500

    added values in it

--- researching_fibonacci_problem_solved.c
+++ researching_fibonacci_problem_solved.c
@@ -17,25 +17,139 @@ int isFibonacci(int n)
 int main()
 {
     int i;
-    int array[10];
+    int array[114] = {
+        7,
+        286,
+        200,
+        176,
+        120,
+        165,
+        206,
+        75,
+        129,
+        109,
+        123,
+        111,
+        43,
+        52,
+        99,
+        128,
+        111,
+        110,
+        98,
+        135,
+        112,
+        78,
+        118,
+        64,
+        77,
+        227,
+        93,
+        88,
+        69,
+        60,
+        34,
+        30,
+        73,
+        54,
+        45,
+        83,
+        182,
+        88,
+        75,
+        85,
+        54,
+        53,
+        89,
+        59,
+        37,
+        35,
+        38,
+        29,
+        18,
+        45,
+        60,
+        49,
+        62,
+        55,
+        78,
+        96,
+        29,
+        22,
+        24,
+        13,
+        14,
+        11,
+        11,
+        18,
+        12,
+        12,
+        30,
+        52,
+        52,
+        44,
+        28,
+        28,
+        20,
+        56,
+        40,
+        31,
+        50,
+        40,
+        46,
+        42,
+        29,
+        19,
+        36,
+        25,
+        22,
+        17,
+        19,
+        26,
+        30,
+        20,
+        15,
+        21,
+        11,
+        8,
+        8,
+        19,
+        5,
+        8,
+        8,
+        11,
+        11,
+        8,
+        3,
+        9,
+        5,
+        4,
+        7,
+        3,
+        6,
+        3,
+        5,
+        4,
+        5,
+        6};
     int count_fib = 0, count_nonfib = 0; // count variables
 
-    for (i = 0; i < 10; ++i)
-    {
-        printf("Enter the number of statements in %d chapter: ", i + 1);
-        scanf("%d", &array[i]);
-    }
-    printf("Recheck your whole array\n");
+    // for (i = 0; i < 114; ++i)
+    // {
+    //     printf("Enter the number of statements in %d chapter: ", i + 1);
+    //     scanf("%d", &array[i]);
+    // }
+    // printf("Recheck your whole array\n");
 
-    for (i = 0; i < 10; i++)
-    {
-        printf(" %d  ", array[i]);
-    }
-    printf("\n ");
+    // for (i = 0; i < 114; i++)
+    // {
+    //     printf(" %d  ", array[i]);
+    // }
+    // printf("\n ");
 
     // Filtering and counting Fibonacci and non-Fibonacci numbers
     printf(" Numbers given below are the fibonacci numbers!:\n");
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 114; i++)
     {
         if (isFibonacci(array[i]))
         {
@@ -46,7 +160,7 @@ int main()
     printf("\n");
 
     printf(" Numbers given below are not fibonacci numbers!\n");
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 114; i++)
     {
         if (!isFibonacci(array[i]))
         {
@@ -67,7 +181,7 @@ int main()
 // 2. take input  whole array numbers that you want to check
 // 3. print them to recheck them
 // 4. filter out fibonacci numbers  out of them
-// 10. print all those numbers that are infact fibonaccinumbers
+// 114. print all those numbers that are infact fibonaccinumbers
 // 6. print all those numbers that are not fibonacci numbers
 
 // then when all of it is done perfectly
